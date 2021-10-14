996d15c87b27f398f7004e670eeb4810a7432523
ngocthanh-shu <ngocthanh.shu@gmail.com>
2021-10-14 16:00:23 +0700

    update

---
+++ assets/js/style.js
@@ -0,0 +1,24 @@
+const buyBtns = document.querySelectorAll('.js-buy-ticket')
+const modal = document.querySelector('.js-modal')
+const modalClose = document.querySelector('.js-modal-close')
+const modalContainer = document.querySelector('.js-modal-container')
+
+
+function showBuyTicket() {
+    modal.classList.add('open')
+}
+
+function hideBuyTicket() {
+    modal.classList.remove('open')
+}
+
+for(const buyBtn of buyBtns) {
+    buyBtn.addEventListener('click',showBuyTicket)
+}
+
+modalClose.addEventListener('click',hideBuyTicket)
+modal.addEventListener('click',hideBuyTicket)
+
+modalContainer.addEventListener('click',function(event) {
+    event.stopPropagation()
+})
