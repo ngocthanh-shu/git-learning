40b7fa6ba2e429f9c04fa7661da4fae3bb4adfc7
ngocthanh-shu <ngocthanh.shu@gmail.com>
2021-10-23 20:32:25 +0700

    update

--- assets/js/reponsive.js
+++ assets/js/reponsive.js
@@ -1,16 +1,33 @@
 var header = document.getElementById('header');
-console.log(header);
+//console.log(header);
 
 var mobileMenu = document.getElementById('mobile-menu');
-console.log(mobileMenu)
+//console.log(mobileMenu)
 var headerHeight = header.clientHeight;
 
-mobileMenu.onclick = function() {
+mobileMenu.onclick = function () {
     var isClose = header.clientHeight === headerHeight;
-    if(isClose){
+    if (isClose) {
         header.style.height = 'auto';
     }
-    else{
+    else {
         header.style.height = null;
     }
+}
+
+var menuItems = document.querySelectorAll('#nav li a[href *= "#"]');
+console.log(menuItems)
+for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+
+    menuItem.onclick = function(event) {
+        var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+        if(isParentMenu){
+            event.preventDefault();
+        }
+        else {
+            header.style.height = null;
+        }
+    }
+
 }
